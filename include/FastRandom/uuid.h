75bfbbcad3a584bf7c1d77bb373c7e81c5060811
ibroheem <movichtech@gmail.com>
2018-02-10 19:07:24 +0100

    Aestethics

---
+++ include/FastRandom/uuid.h
@@ -0,0 +1,92 @@
+/**
+ * Licenced under MIT Licence
+ *
+ * (c) Ludovic 'Archivist' Lagouardette 2018
+ *
+ */
+
+#include "FastRandom/base_prng.h"
+#include <chrono>
+#include <string>
+#include <random>
+
+namespace archivist
+{
+   class fast_uuid {};
+   class balanced_uuid {};
+   class strong_uuid {};
+
+   enum class uuid_modes
+   {
+      balanced, fast, strong
+   };
+
+   struct basic_uuid
+   {
+      static thread_local std::random_device src;
+      static thread_local uint8_t balance;
+      uint64_t state[2]{};
+
+      basic_uuid()
+      {
+         init_default();
+      }
+
+      inline void init_default()
+      {
+         state[0] = prng(9998);
+         state[1] = prng(17);
+      }
+
+      inline void init_strong()
+      {
+         uint32_t internal = src();
+         uint32_t cross = src() | std::chrono::duration_cast< std::chrono::microseconds >(
+                                  std::chrono::system_clock::now().time_since_epoch()).count();
+
+         state[0] = prng(internal);
+         prng_feed(cross);
+         state[1] = prng(cross);
+      }
+
+      bool operator==(const basic_uuid& rhs) const
+      {
+         return (state[0] == rhs.state[0]) && (state[1] == rhs.state[1]);
+      }
+
+      bool operator<(const basic_uuid& rhs) const
+      {
+         return (state[0] < rhs.state[0]) || ((state[1] < rhs.state[1]) && (state[0] == rhs.state[0]));
+      }
+   };
+
+   using uuid = archivist::basic_uuid;
+
+   struct uuid_balanced : public basic_uuid
+   {
+      uuid_balanced()
+      {
+         if(++balance % 128)
+            init_default();
+
+         else
+            init_strong();
+      }
+   };
+
+   struct uuid_fast : public basic_uuid
+   {
+      uuid_fast() : basic_uuid()
+      {
+      }
+   };
+
+   struct uuid_strong : public basic_uuid
+   {
+      uuid_strong()
+      {
+         init_strong();
+      }
+   };
+
+}
